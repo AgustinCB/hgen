756aa640f987bc81e7986682741ec5674d2fc052
Agustin Chiappe Berrini <jnieve@gmail.com>
2017-03-03 08:37:28 -0500

    better data type in population

--- src/Population.hs
+++ src/Population.hs
@@ -2,24 +2,31 @@ module Population () where
 
 import Data.Array
 
-class (Array Integer a) => Population a where
-  fitness :: Num b => Population a -> Integer -> b
-  addPopulation :: Population a -> (Array Integer a) -> Population a
-  individual :: Population a -> Integer -> a
-  allFitness :: Population a -> [b]
-  totalFitness :: Population a -> b
-  best :: Population a -> b
-  average :: Population a -> Double
-  size :: Population a -> Integer
-  forAll :: (a -> IO a) -> Population a -> IO (Population a)
+type Fitness a = Population a -> Integer -> Double
+data Population a = Population [a] (Fitness a)
 
-  addPopulation pop newPop = pop ++ newPop
-  individual pop i = pop ! i
-  allFitness pop = map (fitness pop) [0..(size pop) -1]
-  totalFitness pop = sum $ allFitness pop
-  best pop = maximum $ allFitness pop
-  average pop = (totalFitness pop) / (size pop)
-  size = length pop
-  forAll fn pop = do
-    newPop <- map fn pop
-    return newPop 
+addPopulation :: Population a -> [a] -> Population a
+addPopulation (Population pop fitness) newPop = Population (pop ++ newPop) fitness
+
+individual :: Population a -> Integer -> a
+individual (Population pop _) i = pop ! i
+
+allFitness :: Population a -> [b]
+allFitness (Population pop fitness) = map (fitness pop) [0..(size pop) -1]
+
+totalFitness :: Population a -> b
+totalFitness pop = sum $ allFitness pop
+
+best :: Population a -> b
+best pop = maximum $ allFitness pop
+
+average :: Population a -> Double
+average pop = (totalFitness pop) / (size pop)
+
+size :: Population a -> Integer
+size = length pop
+
+forAll :: (a -> IO a) -> Population a -> IO (Population a)
+forAll fn pop = do
+  newPop <- map fn pop
+  return newPop 
