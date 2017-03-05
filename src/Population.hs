0f07caacd7d98ddb95beb2b924b9203bab10533e
Agustin Chiappe Berrini <jnieve@gmail.com>
2017-03-05 09:36:16 -0500

    Make all compile and think about forAll later

--- src/Population.hs
+++ src/Population.hs
@@ -1,32 +1,29 @@
 module Population () where
 
-import Data.Array
-
-type Fitness a = Population a -> Integer -> Double
+type Fitness a = a -> Double
 data Population a = Population [a] (Fitness a)
 
 addPopulation :: Population a -> [a] -> Population a
 addPopulation (Population pop fitness) newPop = Population (pop ++ newPop) fitness
 
-individual :: Population a -> Integer -> a
-individual (Population pop _) i = pop ! i
+individual :: Population a -> Int -> a
+individual (Population pop _) i = pop!!i
 
-allFitness :: Population a -> [b]
-allFitness (Population pop fitness) = map (fitness pop) [0..(size pop) -1]
+allFitness :: Population a -> [Double]
+allFitness (Population pop fitness) = map fitness pop
 
-totalFitness :: Population a -> b
+totalFitness :: Population a -> Double
 totalFitness pop = sum $ allFitness pop
 
-best :: Population a -> b
+best :: Population a -> Double
 best pop = maximum $ allFitness pop
 
 average :: Population a -> Double
-average pop = (totalFitness pop) / (size pop)
+average pop = (totalFitness pop) / (fromIntegral (size pop))
 
-size :: Population a -> Integer
-size = length pop
+size :: Population a -> Int
+size (Population pop _) = length pop
 
 forAll :: (a -> IO a) -> Population a -> IO (Population a)
 forAll fn pop = do
-  newPop <- map fn pop
-  return newPop 
+  return pop
